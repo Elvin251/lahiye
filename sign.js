9d3233cc003d735e5ea025ae80aa88a94ad43b6c
Elvin251 <emmmdov485@gmail.com>
2025-05-18 01:12:43 +0400

    yeniden

--- sign.js
+++ sign.js
@@ -1,6 +1,6 @@
 let users = JSON.parse(localStorage.getItem("users")) || [];
 let form = document.querySelector("form");
-let inputs = document.querySelectorAll("input");
+let inputs = document.querySelectorAll(".bir");
 
 function validateInput(input) {
     if (input.checkValidity() && input.value.trim() !== "") {
